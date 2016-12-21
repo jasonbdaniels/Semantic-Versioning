bb538dda8504aad5a50b48c77f3874de7786f0f7
Jason Daniels <jasonbdaniels@gmail.com>
2016-12-21 15:34:59 -0500

    Initial commit.

---
+++ Semantic Versioning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Semantic Versioning
+//
+//  Created by Daniels, Jason on 12/21/16.
+//  Copyright © 2016 Daniels, Jason. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
